5b12c68eaf2435c21499a405ddf313e019490fe7
Maged E. William <Maged.EWilliam@Gmail.com>
2017-07-22 23:17:13 +0200

    auto relod wikipedia to mobile

--- popup.js
+++ popup.js
@@ -1,15 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-	    chrome.tabs.getSelected(null, function(tab) {
-	        tabUrl = tab.url;
-	        if( isWikipediaDesktop(tabUrl) ){
-	        	var toMobile = addMToUrl(tabUrl)
-	        	chrome.tabs.update({url: toMobile});
-	        }
-    	});
+// 	chrome.tabs.getSelected(null, function(tab) {
+
+// 	    RunWikipediaTwoMobile(tab);
+// 	});
+
+// }, false);
+
+// chrome.tabs.query({'active': true}, function(tab) {
+  
+  
+// });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { // onUpdated should fire when the selected tab is changed or a link is clicked 
+    chrome.tabs.getSelected(null, function(tab) {
+        myURL = tab.url;
+        RunWikipediaTwoMobile(tab);
+    });
+});
+
+// chrome.tabs.query({
+//   active: true,
+//   currentWindow: true
+// }, function(tabs) {
+//   var tab = tabs[0];
+//   var url = tab.url;
+//   RunWikipediaTwoMobile(tab);
+// });
 
 
-}, false);
+function RunWikipediaTwoMobile(tab){
+
+    tabUrl = tab.url;
+    if( isWikipediaDesktop(tabUrl) ){
+    	var toMobile = addMToUrl(tabUrl)
+    	chrome.tabs.update({url: toMobile});
+    }
+}
 
 function isWikipediaDesktop(url){
 
