704cb500ea96728640d06c07a5e659f4631f3223
Maged E. William <Maged.EWilliam@Gmail.com>
2017-07-22 23:45:10 +0200

    minify script

--- popup.js
+++ popup.js
@@ -1,37 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function() {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
-// 	chrome.tabs.getSelected(null, function(tab) {
-
-// 	    RunWikipediaTwoMobile(tab);
-// 	});
-
-// }, false);
-
-// chrome.tabs.query({'active': true}, function(tab) {
-  
-  
-// });
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { // onUpdated should fire when the selected tab is changed or a link is clicked 
     chrome.tabs.getSelected(null, function(tab) {
-        myURL = tab.url;
         RunWikipediaTwoMobile(tab);
     });
 });
 
-// chrome.tabs.query({
-//   active: true,
-//   currentWindow: true
-// }, function(tabs) {
-//   var tab = tabs[0];
-//   var url = tab.url;
-//   RunWikipediaTwoMobile(tab);
-// });
-
-
 function RunWikipediaTwoMobile(tab){
 
-    tabUrl = tab.url;
+    var tabUrl = tab.url;
     if( isWikipediaDesktop(tabUrl) ){
     	var toMobile = addMToUrl(tabUrl)
     	chrome.tabs.update({url: toMobile});
@@ -47,6 +23,6 @@ function isWikipediaDesktop(url){
 }
 
 function addMToUrl(url){
-
+	
 	return url.replace('wikipedia.org', 'm.wikipedia.org')
 }
