1a996c53412e8bb8d20f1712be98e747afd4334a
Maged E. William <Maged.EWilliam@Gmail.com>
2017-07-23 00:32:14 +0200

    refactor and -r min script

--- popup.js
+++ popup.js
@@ -1,9 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
 	if( tab.url.indexOf('wikipedia.org') == 11 ){
-
 		chrome.tabs.getSelected(null, function(tab) {
-
 			chrome.tabs.update({url: tab.url.replace('wikipedia.org', 'm.wikipedia.org') });
 		});
 	}
