22176c32314c72978db059f5a44d52f1895a82b3
Maged E. William <Maged.EWilliam@gmail.com>
2021-11-30 13:22:34 +0200

    minify the script, making query specific

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,1 @@
-(()=>{
-	const toMobileView = document.querySelector('#footer-places-mobileview a');
-	toMobileView? toMobileView.click() : false;
-})();
+(() => { document.querySelector('.stopMobileRedirectToggle')?.click() })()
